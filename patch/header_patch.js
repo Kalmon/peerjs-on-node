7d2cafa6d02bb6119d2e217b2ba58aea19588374
Mugur Tomita Jr <mugur.tomita@gmail.com>
2023-05-25 15:52:34 +0300

    Patched

--- patch/header_patch.js
+++ patch/header_patch.js
@@ -5,7 +5,7 @@ RTCSessionDescription = wrtc.RTCSessionDescription;
 
 fetch = require('node-fetch');
 WebSocket = require('ws');
-FileReader = require('filereader');
+FileReader = require('./file_reader');
 
 Blob = require('node-blob');
 const blobToArraybuffer = require('blob-to-arraybuffer');
